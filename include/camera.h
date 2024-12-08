f1b0b6c64a8a24e2972ea617a2c27fd5e1ea4ac4
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2024-12-07 22:45:28 -0600

    moved define macros and changed items to linked list

--- include/camera.h
+++ include/camera.h
@@ -4,6 +4,8 @@
 #include <raylib.h>
 #include <raymath.h>
 
+#define CAMERA_FOLLOW_SPEED 0.1f
+
 void CreateCamera(Camera2D *camera, int width, int height);
 void UpdateCameraTarget(Camera2D *camera, Vector2 target, float follow_speed);
 
